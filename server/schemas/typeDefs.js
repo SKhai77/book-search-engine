d4ed1f47a4a91581e39ad5c4b2dcb59d85abe7f8
SKhai77 <orionkhai@gmail.com>
2023-11-01 00:53:48 -0500

    Defined the Query and Mutation types, and tested it

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+// Defining GraphQL type definitions
+const typeDefs = `
+  # Defining a "User" type with it values
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+
+  # Defining "Book" type with it values
+  type Book {
+    authors: [String]
+    description: String
+    bookId: ID!
+    image: String
+    link: String
+    title: String!
+  }
+
+  # Defining an "Auth" type with it values
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  # Defining a "Query" type 'me' to the 'User'
+  type Query {
+    me: User
+  }
+
+  # Defining an input type "SavedBookInput' for saving a book
+  input SavedBookInput {
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  # Defining the "Mutation" types
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(book: SavedBookInput): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+// Exports the "type definitions"
+module.exports = typeDefs;
