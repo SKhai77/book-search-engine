0f6487758bce57471dc0fa80798bc09cda4577b9
SKhai77 <orionkhai@gmail.com>
2023-11-01 00:13:47 -0500

    Updated the file by exporting the typeDefs and resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// Import the required modules
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+
+// Export the modules
+module.exports = { typeDefs, resolvers };
