3e0d62e6962a537f8ffe37fe60dfe4049c93dfb4
SKhai77 <orionkhai@gmail.com>
2023-11-01 23:15:24 -0500

    Added 'bookCount' field

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,14 @@
 // Import GraphQL Apollo/client
-// import { gql } from "@apollo/client";
+import { gql } from "@apollo/client";
 
 //  GraphQL query to fetch the current authenticated user's information
-export const GET_ME = `
+export const GET_ME = gql`
 {
   me {
     _id
     username
     email
+    bookCount
     savedBooks {
       title
       bookId
