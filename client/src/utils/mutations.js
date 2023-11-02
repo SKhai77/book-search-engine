3e7a888fbe79f34f6bab9276ed45b04861fc9fe9
SKhai77 <orionkhai@gmail.com>
2023-11-02 03:36:40 -0500

    Added comments to the codes and changed the REMOVE_BOOK's mutation from  to

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,7 @@
+// Import the 'gq' template literal tag from the apollo/client library
 import { gql } from "@apollo/client";
 
+// Define a GraphQL mutation named LOGIN_USER to login
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -12,6 +14,7 @@ export const LOGIN_USER = gql`
   }
 `;
 
+// Define a GraphQL mutation named ADD_USER to add user
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -25,6 +28,7 @@ export const ADD_USER = gql`
   }
 `;
 
+// Define a GraphQL mutation named SAVE_BOOK to save book
 export const SAVE_BOOK = gql`
   mutation saveBook($book: SavedBookInput) {
     saveBook(book: $book) {
@@ -44,9 +48,10 @@ export const SAVE_BOOK = gql`
   }
 `;
 
+// Define a GraphQL mutation named REMOVE_BOOK to remove book
 export const REMOVE_BOOK = gql`
-  mutation removeBook($book: String!) {
-    removeSkill(book: $book) {
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
       _id
       username
       email
