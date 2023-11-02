7c269e53a972f0c3489c225475b75b900340d6c9
SKhai77 <orionkhai@gmail.com>
2023-11-01 23:38:36 -0500

    UnInstalled the apollo server and Installed the apollo client

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,11 +8,12 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      "/graphql": {
-        target: "http://localhost:3001",
+      // Proxy configuration with graphql file path
+      '/graphql': {
+        target: 'http://localhost:3001',
         secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
-});
+        changeOrigin: true
+      }
+    }
+  }
+})
